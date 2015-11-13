48c34625623816b06a263ef70b87d60b89267fcb
Garry Belka <garry@attel.com>
2015-11-13 10:46:33 -0800

    multi chart, multiple plots on page

--- dj/plotapp/urls.py
+++ dj/plotapp/urls.py
@@ -6,7 +6,7 @@ from plotapp import views
 urlpatterns = [
     url(r'^start/$', views.plotter.StartPlotter),
     url(r'^table/$', views.plotter.PrintTable),
-    url(r'^pizza/$', views.plotter.DrawPizza),
     url(r'^s/$', views.plotter.DrawPlots),
+    url(r'^m/$', views.plotter.DrawMultiPlot),
     url(r'^$', views.plotter.DrawPlot),
 ]
--- dj/plotapp/views.py
+++ dj/plotapp/views.py
@@ -16,19 +16,36 @@ class Plotter(object):
         <script type="text/javascript">
 
           // Load the Visualization API and the package.
-          google.load('visualization', '1.1', {'packages':['line']});
+          google.load('visualization', '1', {'packages':['corechart']});
 
           // Set a callback to run when the Google Visualization API is loaded.
-          google.setOnLoadCallback(drawChart);
+          google.setOnLoadCallback(drawAllCharts);
   """
 
-  FOOTER = """
+  DRAW = """
+            // Set chart options
+            var options = {title: %r};
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.LineChart(
+                document.getElementById(%r));
+            chart.draw(data, options);
+  """
+
+  MID = """
         </script>
       </head>
 
       <body>
-        <!--Div that will hold the  chart-->
-        <div id="chart_div" style="width: 900px; height: 500px"></div>
+  """
+
+  # Div holds the chart.
+  DIV = """
+        <p></p>
+        <div id="%s"></div>
+  """
+
+  FOOTER = """
       </body>
       </html>
   """
@@ -64,12 +81,10 @@ class Plotter(object):
     return HttpResponse('\n'.join(response))
 
   def _GenDataTable(self, chId, start_ms=None, stop_ms=None):
-    """Generates JS for Google Chartsd DataTable object."""
+    """Generates JS for Google Charts DataTable object."""
 
-    js = """
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-    """
+    jslist = ['            // Create the data table (chId %s).' % chId,
+              '            var data = new google.visualization.DataTable();']
     entities = self.dataCollector.GetChannelData(
         chId, start_ms=start_ms, stop_ms=stop_ms)
     column_names = {key for x in entities for key in x}
@@ -79,63 +94,106 @@ class Plotter(object):
     column_names = header
     for name in column_names:
       kind = 'number'
-      js += "            data.addColumn(%r, %r);\n" % (kind, name)
-    rows = []
-    for entity in entities:
-      row = [entity.get(cn, '') for cn in column_names]
-      rows.append(row)
+      jslist.append('            data.addColumn(%r, %r);' % (kind, name))
+      rows = []
+      for entity in entities:
+        row = [entity.get(cn, '') for cn in column_names]
+        rows.append(row)
     rows_repr = ', '.join('%r' % row for row in rows)
-    js += "            data.addRows([%s]);\n" % rows_repr
-    return js, len(column_names)
+    jslist.append('            data.addRows([%s])' % rows_repr)
+    return '\n'.join(jslist), len(column_names)
+
+  def _GenChart(self, chId):
+    jslist = [
+        self.HEADER,
+        '        function drawAllCharts() {',
+        self._GenDataTable(chId)[chId],
+        self.DRAW % ('Chart', 'chart_div'),
+        '        }',
+        self.MID,
+        self.DIV % 'chart_div',
+        self.FOOTER]
+    return '\n'.join(jslist)
+
+  def _GenChartFun(self, chIds, name, title=None, chart_div=None):
+    title = title or 'Chart'
+    chart_div = chart_div or 'chart_div'
+    jslist = [
+        '          function %s() {' % name,
+        '            var datas = [];']
+    jsdata_ncols = [self._GenDataTable(chId) for chId in chIds]
+    for jsdata, ncols in jsdata_ncols:
+        jslist.extend([
+            jsdata,
+            '            datas.push(data);',
+        ])
+    jslist.append('            data = datas[0];')
+    i = -1
+    for jsdata, ncols in jsdata_ncols:
+      i += 1
+      if i == 0:
+        max_ncols = ncols
+        continue
+      jslist.append('            data = google.visualization.data.join('
+                    "data, datas[%d], 'full', [[0, 0]], %r, %r);" % (
+          i, range(1, max_ncols), range(1, ncols)))
+      max_ncols = max(max_ncols, ncols)
+    jslist.extend([
+        self.DRAW % (title, chart_div),
+        '          }'])
+    return '\n'.join(jslist)
+
+  def _GenMultiChart(self, chIds):
+    jslist = [self.HEADER]
+    jslist.append(self._GenChartFun(chIds, 'drawAllCharts'))
+    jslist.extend([
+        self.MID,
+        self.DIV % 'chart_div',
+        self.FOOTER])
+    return '\n'.join(jslist)
+
+  def _GenMultiCharts(self, chDescList):
+    """
+    Args:
+      chDescList: list<{chIds: [], title: , div: }>, Chart descriptors.
+
+    Returns:
+      str, Generated Javascript.
+    """
+    jslist = [self.HEADER]
+    for i, chDesc in enumerate(chDescList):
+      jslist.append(self._GenChartFun(
+          chDesc['chIds'],
+          'drawChart%d' % i,
+          chDesc.get('title', 'Chart%d' % i),
+          chDesc.get('div', 'chart_div%d' % i)))
+    jslist.append('          function drawAllCharts() {')
+    jslist.extend(['            drawChart%d();' % i
+                   for i in xrange(len(chDescList))])
+    jslist.extend([
+        '          }',
+        self.MID])
+    for i, chDesc in enumerate(chDescList):
+      jslist.append(self.DIV % chDesc.get('div', 'chart_div%d' % i))
+    jslist.append(self.FOOTER)
+    return '\n'.join(jslist)
 
   def DrawPlot(self, request):
     """Draws a plot for a single channel."""
-    js = self.HEADER
-    js += """        function drawChart() {
-    """
-    js0, _ = self._GenDataTable(0)
-    js += js0
-    js += """
-            // Set chart options
-            var options = {title: 'Chart'};
+    return HttpResponse(self._GenChart(0))
 
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.charts.Line(
-                document.getElementById('chart_div'));
-            chart.draw(data, options);
-          }
-    """
-    js += self.FOOTER
-
-    return HttpResponse(js)
-
+  def DrawMultiPlot(self, request):
+    """Draws a plot for several channels."""
+    return HttpResponse(self._GenMultiChart([0, 1]))
 
   def DrawPlots(self, request):
-    """Draws a plot for 2 channels."""
-    js = self.HEADER
-    js += """        function drawChart() {
-    """
-    js0, ncols0 = self._GenDataTable(0)
-    js += js0
-    js += '        data0 = data;\n'
-    js1, ncols1 = self._GenDataTable(1)
-    js += js1
-    js += """
-            data = google.visualization.data.join(data0, data,
-                'full', [[0, 0]], %r, %r);
-    """ % (range(1, ncols0), range(1, ncols1))
-    js += """
-            // Set chart options
-            var options = {title: 'Chart'};
+    """Draws multiple plots with multiple charts each."""
+    return HttpResponse(self._GenMultiCharts([
+      {'chIds': [0, 1]},
+      {'chIds': [0, 1], 'title': 'ch01', 'div': 'ch_div1'},
+      {'chIds': [1]},
+      {'chIds': [1], 'title': 'ch1', 'div': 'ch_div3'},
+    ]))
 
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.charts.Line(
-                document.getElementById('chart_div'));
-            chart.draw(data, options);
-          }
-    """
-    js += self.FOOTER
-
-    return HttpResponse(js)
 
 plotter = Plotter()
